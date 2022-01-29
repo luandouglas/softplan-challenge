8542df8840123e5f3a9d10648160ed79ba7c71ab
Luan Douglas Maia da Silva <douglasmaia.mn.sl@gmail.com>
2022-01-29 08:55:34 -0300

    :tada: Initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home } from '../pages';
+
+export const Router = () => {
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<div>Page Not Found</div>} />
+      </Routes>
+    </div>
+  );
+};
